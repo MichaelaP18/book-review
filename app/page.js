c9bc7f7e60b67ea667012a2a0d4ef68d1582ec34
Garth Carey <Garthcarey0862@gmail.com>
2024-12-08 17:12:47 -0700

    added styles for the login page

--- app/page.js
+++ app/page.js
@@ -17,7 +17,7 @@ export default function Page() {
   };
 
   return (
-    <div className='flex flex-col min-h-screen'>
+    <div className='flex flex-col h-screen'>
       <header className='bg-purple-500 text-white p-8 shadow-md'>
         <div className='container mx-auto flex justify-between items-center'>
           <h1 className='text-3xl font-bold'>Bookish Buzz</h1>
@@ -30,38 +30,48 @@ export default function Page() {
                 Home
               </Link>
             </li>
-            <li>
-              <Link
-                href='/browse'
-                className='hover:underline'>
-                Browse
-              </Link>
-            </li>
           </ul>
         </div>
       </header>
 
       <main className='flex-grow p-6'>
         {user ? (
-          <div className='flex flex-col items-center justify-center h-screen'>
-            <p className='mb-4 p-4'>Welcome, {user.displayName}</p>
-            <button
-              className='mb-4 hover:underline'
-              onClick={signOut}>
-              Sign Out
-            </button>
-            <p className='text-center p-8'>Welcome to Bookish Buzz!</p>
+          <div className='flex flex-col items-center justify-center'>
+            <div className='flex flex-col items-center border-4 border-yellow-200/80 bg-purple-500/75 rounded-xl drop-shadow-xl absolute top-1/3'>
+              <p className='text-center p-8 text-3xl  text-slate-100'>
+                Welcome to Bookish Buzz!
+              </p>
+              <p className='mb-4 p-4 text-3xl text-slate-100'>
+                Welcome, {user.displayName}
+              </p>
+              <div className=' flex  justify-center border-2 hover:border-yellow-200 border-white bg-purple-700/30 rounded-md my-4 w-32'>
+                <Link
+                  href='/browse'
+                  className='hover:underline  text-2xl  text-slate-100'>
+                  Browse
+                </Link>
+              </div>
+              <button
+                className='mb-4 hover:underline border-2 border-white hover:border-purple-600 bg-yellow-200 rounded-md p-2'
+                onClick={signOut}>
+                Sign Out
+              </button>
+            </div>
           </div>
         ) : (
-          <div className='flex flex-col items-center justify-center min-h-screen'>
-            <h1 className='font-cursive text-3xl text-center p-4'>
-              Sorry, you need to sign in first!
-            </h1>
-            <button
-              className='text-center hover:underline mt-4'
-              onClick={signIn}>
-              Sign In
-            </button>
+          <div className='flex w-1/2 h-2/3  justify-center  items-center absolute left-1/4 top-1/5'>
+            <div
+              className='flex flex-col
+            justify-center items-center border-4 border-yellow-200/80 bg-purple-500/75 rounded-xl drop-shadow-xl w-2/3 h-1/3'>
+              <h1 className='font-cursive text-3xl text-center p-4  text-slate-100'>
+                Sorry, you need to sign in first!
+              </h1>
+              <button
+                className='text-center hover:underline mt-4 border-white border-2 hover:border-yellow-200 bg-purple-700/30 rounded-md p-2 m-5 text-slate-100'
+                onClick={signIn}>
+                Sign In
+              </button>
+            </div>
           </div>
         )}
       </main>
